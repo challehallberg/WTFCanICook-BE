a4a721192d1d4ef75925e10084324a26a000dc11
challehallberg <challe.1997@live.se>
2025-03-04 21:56:15 +0100

    Remove ping Endpoint and return correct objects

--- src/main/java/com/cb/recipe/endpoint/Endpoint.java
+++ src/main/java/com/cb/recipe/endpoint/Endpoint.java
@@ -1,20 +1,20 @@
 package com.cb.recipe.endpoint;
 
 import com.cb.recipe.handler.RecipeHandler;
+import com.cb.recipe.model.instruction.Instruction;
 import com.cb.recipe.model.recipe.Recipe;
 import io.swagger.v3.oas.annotations.Operation;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/recipes")
 @CrossOrigin
-@Slf4j
 public class Endpoint {
     private final RecipeHandler recipeHandler;
 
@@ -24,9 +24,9 @@ public class Endpoint {
 
     @PostMapping(value = "/getRecipes", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "Get recipes from uploaded images")
-    public ResponseEntity<?> getRecipes(@RequestPart("files") final MultipartFile... files) {
+    public ResponseEntity<List<Recipe>> getRecipes(@RequestPart("files") final MultipartFile... files) {
         if (files == null) {
-            return ResponseEntity.badRequest().body("File is empty");
+            return ResponseEntity.badRequest().body(Collections.emptyList());
         }
         final List<Recipe> recipes = recipeHandler.getRecipes(files);
 
@@ -35,14 +35,8 @@ public class Endpoint {
 
     @GetMapping("/instruction/{recipeId}")
     @Operation(summary = "Get instructions for a specific recipe")
-    public ResponseEntity<?> getInstructions(@PathVariable("recipeId") final int recipeId) {
+    public ResponseEntity<List<Instruction>> getInstructions(@PathVariable("recipeId") final int recipeId) {
         return ResponseEntity.ok(recipeHandler.getInstructions(recipeId));
-    }
-
-    @PostMapping("/ping")
-    @Operation(summary = "Ping Pong!")
-    public ResponseEntity<?> pong() {
-        return ResponseEntity.ok("Pong");
     }
 
 }
